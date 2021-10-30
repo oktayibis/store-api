2806caf606446cd4b3e35734cac72330ab88ea6d
Oktay Ibis <73842733+oktay-ibis@users.noreply.github.com>
2021-10-31 00:52:01 +0300

    token error messages fixed

--- src/middlewares/admin.auth.ts
+++ src/middlewares/admin.auth.ts
@@ -1,25 +1,25 @@
-import express from "express"
-import jwt from "jsonwebtoken"
-import { ErrorResponse } from "../utils/Helpers"
+import express from "express";
+import jwt from "jsonwebtoken";
+import { ErrorResponseWithMessage } from "../utils/Helpers";
 
-export default function CheckAdminTokenValid(req: express.Request, res: express.Response, next:express.NextFunction) {
-  const authHeader = req.headers.authorization
+export default function CheckAdminTokenValid(req: express.Request, res: express.Response, next: express.NextFunction) {
+  const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).send(ErrorResponse("Token bilgisi gelmedi"))
+    return res.status(401).send(ErrorResponseWithMessage("Token bilgisi gelmedi"));
   }
   try {
-   const token = authHeader.split(" ")[1] // take token from header
-    const verifyToken = jwt.verify(token, process.env.SECRET)
+    const token = authHeader.split(" ")[1]; // take token from header
+    const verifyToken = jwt.verify(token, process.env.SECRET);
 
     const { userId, role } = verifyToken as any;
     if (role !== "admin") {
-      return res.status(401).send(ErrorResponse("Bu işlem için yetkiniz yok."))
+      return res.status(401).send(ErrorResponseWithMessage("Bu işlem için yetkiniz yok."));
     }
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore
+    // @ts-ignore
     req.user = { userId, role };
-    next()
- } catch (error) {
-   return res.status(401).send(ErrorResponse("Token geçersiz, tekrar giriş yapınız"))
- }
+    next();
+  } catch (error) {
+    return res.status(401).send(ErrorResponseWithMessage("Token geçersiz, tekrar giriş yapınız"));
+  }
 }
